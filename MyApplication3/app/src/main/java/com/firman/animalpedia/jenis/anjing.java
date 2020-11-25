36a888a974e4077b71c4dfa6e7c66dce3498a893
Taniajayadi12 <taniajayadi861@gmail.com>
2020-11-24 23:15:58 -0800

    tugas

---
+++ MyApplication3/app/src/main/java/com/firman/animalpedia/jenis/anjing.java
@@ -0,0 +1,8 @@
+package com.firman.animalpedia.jenis;
+
+public class anjing extends hewan {
+
+    public anjing(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Anjing",ras,asal,deskripsi,drawableRes);
+    }
+}
---
+++ MyApplication3/app/src/main/java/com/firman/animalpedia/jenis/kucing.java
@@ -0,0 +1,9 @@
+package com.firman.animalpedia.jenis;
+
+public class kucing extends hewan {
+
+    public kucing(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Kucing",ras,asal,deskripsi,drawableRes);
+    }
+
+}
